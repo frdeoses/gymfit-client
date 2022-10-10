11fae92297058a91512f5ac87bada00ab1f4446d
frdeoses <fdoses@outlook.es>
2022-10-10 21:48:55 +0200

    Cretae component "login"

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/app/model/user/usuario.interface';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  user: IUser = {
+    id: undefined,
+    name: '',
+    userName: '',
+    password: '',
+    surname: '',
+    email: '',
+    birthDate: new Date(),
+    height: undefined,
+    phone: '',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
