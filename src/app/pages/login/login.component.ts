350da89c7e6ac184ec2c3bd0581eceb42a47ec64
frdeoses <fdoses@outlook.es>
2022-10-24 18:38:19 +0200

    Update loginComponent

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,8 @@
+import { tokenName } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { IUser } from 'src/app/model/user/usuario.interface';
+import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,19 +10,45 @@ import { IUser } from 'src/app/model/user/usuario.interface';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  user: IUser = {
-    id: undefined,
-    name: '',
-    userName: '',
+  loginData = {
+    username: '',
     password: '',
-    surname: '',
-    email: '',
-    birthDate: new Date(),
-    height: undefined,
-    phone: '',
   };
 
-  constructor() {}
+  constructor(private snack: MatSnackBar, private loginService: LoginService) {}
 
   ngOnInit(): void {}
+
+  formSubmit() {
+    debugger;
+    // console.log('Click botton de login');
+
+    if (
+      this.loginData.username == null ||
+      this.loginData.username.trim() == ''
+    ) {
+      this.snack.open('Introduce el usuario !!', 'Aceptar', {
+        duration: 3000,
+      });
+      return;
+    }
+    if (
+      this.loginData.password == null ||
+      this.loginData.password.trim() == ''
+    ) {
+      this.snack.open('Introduce la contraseña !!', 'Aceptar', {
+        duration: 3000,
+      });
+      return;
+    }
+
+    this.loginService.generateToken(this.loginData).subscribe(
+      (data: any) => {
+        console.log(data);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
