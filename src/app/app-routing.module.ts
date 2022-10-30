e88d49c109c1199cc593dc6f7df55c027a302356
frdeoses <fdoses@outlook.es>
2022-10-30 15:31:03 +0100

    add guard in routing and declared component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/admin/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { UserDashboardComponent } from './pages/user/user-dashboard.component';
+import { UserGuard } from './services/guards/user/user.guard';
 
 const routes: Routes = [
   {
@@ -24,6 +27,17 @@ const routes: Routes = [
     path: 'register',
     component: RegisterComponent,
     pathMatch: 'full',
+  },
+  {
+    path: 'admin',
+    component: DashboardComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'user-dashboard',
+    component: UserDashboardComponent,
+    pathMatch: 'full',
+    canActivate: [UserGuard],
   },
 ];
 
