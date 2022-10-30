acbe4344aae41656fada51e39083ed824b6b4b9f
frdeoses <fdoses@outlook.es>
2022-10-30 15:26:25 +0100

    Create component admin and guard

---
+++ src/app/services/admin.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from './login/login.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminGuard implements CanActivate {
+  constructor(private loginService: LoginService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    if (
+      this.loginService.isLoggedIn() &&
+      this.loginService.getUserRoles() == 'ADMIN'
+    ) {
+      return true;
+    }
+
+    this.router.navigate(['login']);
+    return false;
+  }
+}
