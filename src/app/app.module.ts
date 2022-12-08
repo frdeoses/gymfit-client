f0a775f9019512dea3bc33f52c91030c9f0f36f4
frdeoses <fdoses@outlook.es>
2022-12-08 13:08:06 +0100

    add MatListModule and update dashboard dir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatNativeDateModule, NativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { SidebarComponent } from './pages/admin/sidebar/sidebar.component';
 import { HomeComponent } from './pages/home/home.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { authInterceptorProviders } from './services/auth.interceptor';
-import { DashboardComponent } from './pages/admin/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RegisterComponent } from './pages/register/register.component';
 import { UserDashboardComponent } from './pages/user/user-dashboard.component';
+import { authInterceptorProviders } from './services/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -51,6 +54,7 @@ import { UserDashboardComponent } from './pages/user/user-dashboard.component';
     FormsModule,
     MatToolbarModule,
     MatIconModule,
+    MatListModule,
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent],
