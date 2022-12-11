51c35cf74bcbec2b520f9933f6276adb45bb8409
frdeoses <fdoses@outlook.es>
2022-12-11 18:03:08 +0100

    Update profile components

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { IUser } from 'src/app/interfaces/user/usuario.interface';
+import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  user: IUser = {
+    id: undefined,
+    name: '',
+    username: '',
+    password: '',
+    userRols: [],
+    surname: '',
+    email: '',
+    birthDate: new Date(),
+    height: undefined,
+    phone: '',
+    authorities: [
+      {
+        authority: ' ',
+      },
+    ],
+  };
 
-  constructor() { }
+  constructor(private loginService: LoginService) {}
 
   ngOnInit(): void {
+    this.user = this.loginService.getUser();
   }
-
 }
