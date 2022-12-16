cfb0f54b014f2e7d67cd2ee0e0e87758d7e03885
frdeoses <fdoses@outlook.es>
2022-12-16 18:26:56 +0100

    Create components "view-events" and add styles

---
+++ src/app/pages/admin/view-events/view-events.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { EventService } from 'src/app/services/event/event.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-view-events',
+  templateUrl: './view-events.component.html',
+  styleUrls: ['./view-events.component.css'],
+})
+export class ViewEventsComponent implements OnInit {
+  events: any = [];
+  constructor(private eventService: EventService) {}
+
+  ngOnInit(): void {
+    this.eventService.listEvents().subscribe(
+      (data: any) => {
+        this.events = data;
+        console.log(data);
+      },
+      (error) => {
+        console.error(error);
+        Swal.fire('Error!!', 'Error al cargar los eventos...', 'error');
+      }
+    );
+  }
+
+  deleteEvent(eventId: any) {
+    Swal.fire({
+      title: 'Eliminar evento',
+      text: '¿Estas seguro de eliminar el evento?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Eliminar',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.eventService.deleteEvent(eventId).subscribe(
+          () => {
+            // this.events = this.events.filter((event: any) => {
+            //   event.id != eventId;
+            // });
+
+            Swal.fire(
+              'Examen eliminado',
+              'El examen ha sido eliminado correctamente...',
+              'success'
+            );
+
+            // TODO: RECARGAR TODOS LOS EVENTOS NUEVAMENTE
+            // window.location.reload();
+            this.eventService.listEvents().subscribe(
+              (data: any) => {
+                this.events = data;
+                console.log(data);
+              },
+              (error) => {
+                console.error(error);
+                Swal.fire('Error!!', 'Error al cargar los eventos...', 'error');
+              }
+            );
+          },
+          (error) => {
+            console.error(error);
+            Swal.fire('Error!!', 'Error al eliminar el evento...', 'error');
+          }
+        );
+      }
+    });
+  }
+}
