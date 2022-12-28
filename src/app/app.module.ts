8eac4efffb770b9735c82ef233261b5d8cc01934
frdeoses <fdoses@outlook.es>
2022-12-28 13:26:49 +0100

    Declare component and import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,28 +11,37 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CreateEventComponent } from './pages/admin/create-event/create-event.component';
+import { CreateMachineComponent } from './pages/admin/create-machine/create-machine.component';
+import { CreateTableComponent } from './pages/admin/create-table/create-table.component';
+import { CreateTrainingComponent } from './pages/admin/create-training/create-training.component';
 import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
+import { EditEventComponent } from './pages/admin/edit-event/edit-event.component';
 import { SidebarComponent } from './pages/admin/sidebar/sidebar.component';
+import { ViewEventsComponent } from './pages/admin/view-events/view-events.component';
+import { ViewMachinesComponent } from './pages/admin/view-machines/view-machines.component';
+import { ViewTablesComponent } from './pages/admin/view-tables/view-tables.component';
+import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { UserDashboardComponent } from './pages/user/user-dashboard.component';
 import { authInterceptorProviders } from './services/auth.interceptor';
-import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
-import { ViewTablesComponent } from './pages/admin/view-tables/view-tables.component';
-import { CreateTableComponent } from './pages/admin/create-table/create-table.component';
-import { ViewEventsComponent } from './pages/admin/view-events/view-events.component';
-import { CreateEventComponent } from './pages/admin/create-event/create-event.component';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSelectModule } from '@angular/material/select';
-import { EditEventComponent } from './pages/admin/edit-event/edit-event.component';
+import { ViewTrainingComponent } from './pages/admin/view-training/view-training.component';
+import { EditTrainingComponent } from './pages/admin/edit-training/edit-training.component';
+import { MatBadgeModule } from '@angular/material/badge';
+import { EditGymMachinesComponent } from './pages/admin/edit-gym-machines/edit-gym-machines.component';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -51,6 +60,12 @@ import { EditEventComponent } from './pages/admin/edit-event/edit-event.componen
     ViewEventsComponent,
     CreateEventComponent,
     EditEventComponent,
+    ViewMachinesComponent,
+    CreateMachineComponent,
+    CreateTrainingComponent,
+    ViewTrainingComponent,
+    EditTrainingComponent,
+    EditGymMachinesComponent,
   ],
   imports: [
     BrowserModule,
@@ -71,6 +86,9 @@ import { EditEventComponent } from './pages/admin/edit-event/edit-event.componen
     MatListModule,
     MatSlideToggleModule,
     MatSelectModule,
+    MatBadgeModule,
+    MatChipsModule,
+    MatTooltipModule,
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent],
