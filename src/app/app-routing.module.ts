1c9e3492f4cca2ecd8572933a386abc7dad053ae
frdeoses <fdoses@outlook.es>
2022-12-28 13:28:01 +0100

    add dir

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateEventComponent } from './pages/admin/create-event/create-event.component';
+import { CreateMachineComponent } from './pages/admin/create-machine/create-machine.component';
 import { CreateTableComponent } from './pages/admin/create-table/create-table.component';
+import { CreateTrainingComponent } from './pages/admin/create-training/create-training.component';
 import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
 import { EditEventComponent } from './pages/admin/edit-event/edit-event.component';
+import { EditGymMachinesComponent } from './pages/admin/edit-gym-machines/edit-gym-machines.component';
+import { EditTrainingComponent } from './pages/admin/edit-training/edit-training.component';
 import { ViewEventsComponent } from './pages/admin/view-events/view-events.component';
+import { ViewMachinesComponent } from './pages/admin/view-machines/view-machines.component';
 import { ViewTablesComponent } from './pages/admin/view-tables/view-tables.component';
+import { ViewTrainingComponent } from './pages/admin/view-training/view-training.component';
 import { WelcomeComponent } from './pages/admin/welcome/welcome.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -62,6 +68,30 @@ const routes: Routes = [
       {
         path: 'events/:eventId',
         component: EditEventComponent,
+      },
+      {
+        path: 'gym-machines',
+        component: ViewMachinesComponent,
+      },
+      {
+        path: 'gym-machines/:gymMachineId',
+        component: EditGymMachinesComponent,
+      },
+      {
+        path: 'create-gym-machines',
+        component: CreateMachineComponent,
+      },
+      {
+        path: 'trainings',
+        component: ViewTrainingComponent,
+      },
+      {
+        path: 'create-training',
+        component: CreateTrainingComponent,
+      },
+      {
+        path: 'trainings/:trainingId',
+        component: EditTrainingComponent,
       },
     ],
   },
