f7f2d96b87879f573a11696ca0ee200467149096
frdeoses <fdoses@outlook.es>
2023-02-26 19:42:12 +0100

    add modal delete and add mode edit/consult

--- src/app/pages/admin/view-tables/view-tables.component.ts
+++ src/app/pages/admin/view-tables/view-tables.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ITrainingTable } from 'src/app/interfaces/training-table/trainingTable.interface';
 import { TablesService } from 'src/app/services/tables/tables.service';
 import Swal from 'sweetalert2';
@@ -8,47 +9,9 @@ import Swal from 'sweetalert2';
   templateUrl: './view-tables.component.html',
   styleUrls: ['./view-tables.component.css'],
 })
-export class ViewTablesComponent implements OnInit {
-  // tables = [
-  //   {
-  //     idUser: '63162e71413c66181c52ba04',
-  //     name: 'Tabla 1',
-  //     description: 'Esto es una tabla para trabjar pecho',
-  //     creationDate: 6666666666,
-  //     typeTraining: 'Pecho',
-  //     initDate: 444444444444,
-  //     endDate: 8888888888,
-  //     trainingDuration: 3,
-  //     breakTime: 45,
-  //     observation: 'Test',
-  //   },
-  //   {
-  //     idUser: '63162e71413c66181c52ba04',
-  //     name: 'Tabla 2',
-  //     description: 'Esto es una tabla para trabjar espalda',
-  //     creationDate: 6666666666,
-  //     typeTraining: 'Espalda',
-  //     initDate: 444444444444,
-  //     endDate: 8888888888,
-  //     trainingDuration: 3,
-  //     breakTime: 45,
-  //     observation: 'Test',
-  //   },
-  //   {
-  //     idUser: '63162e71413c66181c52ba04',
-  //     name: 'Tabla 3',
-  //     description: 'Esto es una tabla para trabjar piernas',
-  //     creationDate: 6666666666,
-  //     typeTraining: 'Piernas',
-  //     initDate: 444444444444,
-  //     endDate: 8888888888,
-  //     trainingDuration: 3,
-  //     breakTime: 45,
-  //     observation: 'Test',
-  //   },
-  // ];
-
+export class ViewTablesComponent implements OnInit, OnDestroy {
   tables: ITrainingTable[] = [];
+  subscription: Subscription = new Subscription();
 
   constructor(private tableService: TablesService) {}
 
@@ -63,5 +26,68 @@ export class ViewTablesComponent implements OnInit {
         Swal.fire('Error!!', 'Error al cargar las tablas', 'error');
       }
     );
+    this.subscription = this.tableService.refresh$.subscribe(() => {
+      this.tableService.listTrainingTable().subscribe(
+        (data: ITrainingTable[]) => {
+          this.tables = data;
+          console.log(data);
+        },
+        (error) => {
+          console.error(error);
+          Swal.fire('Error!!', 'Error al cargar los eventos...', 'error');
+        }
+      );
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  deleteGymMachine(trainingTableId: string) {
+    Swal.fire({
+      title: 'Eliminar tabla de entrenamiento',
+      text: '¿Estas seguro de que quieres eliminar la siguiente tabla de entrenamiento?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Eliminar',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.tableService.deleteTrainingTable(trainingTableId).subscribe(
+          (trainingTableId: string) => {
+            Swal.fire(
+              'Tabla de entrenamiento eliminada',
+              'La tabla de entrenamiento ha sido eliminado correctamente...',
+              'success'
+            );
+          },
+          (error) => {
+            console.error(error);
+            Swal.fire(
+              'Error',
+              'Error al eliminar la tabla de entrenamiento del sistema...',
+              'error'
+            );
+          }
+        );
+      }
+    });
+  }
+
+  /**
+   * Entrar en modo edicion
+   */
+  modeEdit() {
+    this.tableService.modeEdit('yes');
+  }
+
+  /**
+   * Entrar en modo consulta
+   */
+  modeConsult() {
+    this.tableService.modeEdit('no');
   }
 }
