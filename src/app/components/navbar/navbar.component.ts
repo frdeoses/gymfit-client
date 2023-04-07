fe2c324de21c0c3e1ae8c8a481b32df7b4146fd4
frdeoses <fdoses@outlook.es>
2023-04-07 19:01:11 +0200

    add new icon in navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,6 +9,7 @@ import { LoginService } from 'src/app/services/login/login.service';
 })
 export class NavbarComponent implements OnInit {
   isLoggedIn: boolean = false;
+  role: string = '';
   user: IUser = {
     id: undefined,
     name: '',
@@ -31,9 +32,23 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
     this.user = this.loginService.getUser();
+    if (
+      this.user.userRols !== undefined &&
+      this.user.userRols.length > 0 &&
+      this.user.userRols[0].roleList !== undefined
+    ) {
+      this.role = this.user.userRols[0].roleList;
+    }
     this.loginService.loginStatusSubject.subscribe((data) => {
       this.isLoggedIn = this.loginService.isLoggedIn();
       this.user = this.loginService.getUser();
+      if (
+        this.user &&
+        this.user.userRols !== undefined &&
+        this.user.userRols[0].roleList !== undefined
+      ) {
+        this.role = this.user.userRols[0].roleList;
+      }
     });
   }
 
