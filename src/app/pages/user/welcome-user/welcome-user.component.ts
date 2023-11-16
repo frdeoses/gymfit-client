75548b5a11a8b9afb3fca0098ad17db9015bd573
frdeoses <frdeosesfuentelu@gmail.com>
2023-11-16 19:36:26 +0100

    add notification in user

--- src/app/pages/user/welcome-user/welcome-user.component.ts
+++ src/app/pages/user/welcome-user/welcome-user.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationService } from 'src/app/services/notification/notification.service';
 
 @Component({
   selector: 'app-welcome-user',
   templateUrl: './welcome-user.component.html',
-  styleUrls: ['./welcome-user.component.css']
+  styleUrls: ['./welcome-user.component.css'],
 })
 export class WelcomeUserComponent implements OnInit {
-
-  constructor() { }
+  constructor(private notificationService: NotificationService) {}
 
   ngOnInit(): void {
+    const notifications =
+      this.notificationService.getNotificationsFromLocalStorage();
+    this.notificationService.separateNotifications(notifications);
+    this.notificationService.clearOldNotifications();
   }
-
 }
