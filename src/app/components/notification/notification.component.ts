93ed9acf6da156084783a4e0774c1202cb0828c5
frdeoses <frdeosesfuentelu@gmail.com>
2023-11-16 19:34:14 +0100

    Update notification component

--- src/app/components/notification/notification.component.ts
+++ src/app/components/notification/notification.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { INotification } from 'src/app/interfaces/notification.interface';
+import { LoginService } from 'src/app/services/login/login.service';
 import { NotificationService } from 'src/app/services/notification/notification.service';
 
 @Component({
@@ -9,44 +11,52 @@ import { NotificationService } from 'src/app/services/notification/notification.
   styleUrls: ['./notification.component.css'],
 })
 export class NotificationComponent implements OnInit {
-  mockNotifications = [
-    {
-      title: 'Nueva Maquina',
-      description: 'Se ha creado una nueva maquina!! ',
-      date: new Date(),
-      read: false,
-    },
-    {
-      title: 'Nuevo Ejercicio',
-      description: 'Se ha creado un nuevo ejercicio!! ',
-      date: new Date(),
-      read: true,
-    },
-    {
-      title: 'Nueva Tabla de entrenamiento',
-      description: 'Se ha creado una nueva tabla de entrenamiento!! ',
-      date: new Date(),
-      read: false,
-    },
-  ];
+  newNotificationsCount: number = 0;
 
   newNotifications$ = this.notificationService.newNotifications$;
   oldNotifications$ = this.notificationService.oldNotifications$;
 
-  constructor(private notificationService: NotificationService) {}
+  constructor(
+    private notificationService: NotificationService,
+    private loginService: LoginService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     const notifications =
       this.notificationService.getNotificationsFromLocalStorage();
     this.notificationService.separateNotifications(notifications);
     this.notificationService.clearOldNotifications();
+    this.notificationService.newNotifications$.subscribe((notifications) => {
+      this.newNotificationsCount = notifications.length;
+    });
   }
 
+  /**
+   *
+   * @param n
+   */
   markRead(n: INotification) {
     this.notificationService.markNotificationAsRead(n);
   }
 
   deleteNotification(notification: INotification) {
     this.notificationService.deleteNotification(notification);
+  }
+
+  goToPage(notification: INotification): void {
+    const role = this.loginService.getCurrentUserRole();
+    this.notificationService.markNotificationAsRead(notification);
+    notification.page =
+      role === 'ADMIN'
+        ? '/admin' + notification.page
+        : '/user-dashboard' + notification.page;
+    debugger;
+    this.router.navigate([notification.page]); // Usa el método navigate para redirigir a la página
+  }
+
+  shouldShowButton(page: string): boolean {
+    debugger;
+    return page.trim() !== '';
   }
 }
