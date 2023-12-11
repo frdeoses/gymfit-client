780edc87f02ebea21bc1f4b1e1aaa5a6fa4d01b5
fdeoses <frdeosesfuentelu@gmail.com>
2023-12-11 12:41:47 +0100

    add new field 'Notification' to Sidebard

--- src/app/pages/admin/sidebar/sidebar.component.ts
+++ src/app/pages/admin/sidebar/sidebar.component.ts
@@ -1,18 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { LoginService } from 'src/app/services/login/login.service';
+import { NotificationService } from 'src/app/services/notification/notification.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent implements OnInit {
-  constructor(public loginService: LoginService) {}
+export class SidebarComponent implements OnInit, OnDestroy {
 
-  ngOnInit(): void {}
+
+  newNotificationsCount: number = 0;
+  subscription: Subscription;
+  constructor(public loginService: LoginService, 
+    public notificationService: NotificationService) {
+
+    this.subscription = new Subscription();
+  }
+
+  ngOnInit(): void {
+
+    this.subscription = this.notificationService.newNotifications$.subscribe(
+      (notifications) => {
+        this.newNotificationsCount = notifications.length;
+        console.log('Notificaciones nuevas recibidas:', notifications);
+      }
+    );
+  }
 
   public logout() {
     this.loginService.logout();
     window.location.reload();
+  }
+
+  updateNotificationCount() {
+    this.notificationService.newNotifications$.subscribe((notifications) => {
+      this.newNotificationsCount = notifications.length;
+    });
+  }
+
+  ngOnDestroy(): void {
+    console.log(this.subscription);
+
+    if (this.subscription) {
+      console.log(this.subscription);
+      this.subscription.unsubscribe();
+    }
   }
 }
